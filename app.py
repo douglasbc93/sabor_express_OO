033396a103eec3977e72a09514c647ae524f05f7
Douglas <138608431+douglasbc93@users.noreply.github.com>
2024-01-30 12:38:27 -0300

    Atualização e criação de novos arquivos com o decorrer do curso

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+
+
+# restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
+# restaurante_japones = Restaurante('Japa', 'Japonesa')
+# restaurante_mexicano.alternar_estado()
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
-restaurante_japones = Restaurante('Japa', 'Japonesa')
-restaurante_mexicano.alternar_estado()
-
-restaurante_praca.receber_avaliacao('Douglas', 10)
-restaurante_praca.receber_avaliacao('Daniel', 8)
-restaurante_praca.receber_avaliacao('Tamara', 2)
+bebida_suco = Bebida('Suco de Melancia', 5.00, 'grande')
+prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+bebida_suco.aplicar_desconto()
+prato_paozinho.aplicar_desconto()
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
